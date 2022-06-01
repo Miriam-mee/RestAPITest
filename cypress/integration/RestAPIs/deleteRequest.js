a9211d55b41fa6d61065bc5cdb1e8edb4f7fab7e
Miriam-mee <sundaymiriam93@yahoo.com>
2022-06-01 09:27:34 +0100

    delete request test
    Test for delete request

---
+++ cypress/integration/RestAPIs/deleteRequest.js
@@ -0,0 +1,25 @@
+/// <reference types ='cypress' />
+
+describe("Test APIS", () => {
+  it("Delete an employee record", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://dummy.restapiexample.com/api/v1/delete/3941",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.message).contains(
+        "Successfully! Record has been deleted"
+      );
+    });
+
+    cy.request({
+      method: "GET",
+      url: "https://dummy.restapiexample.com/api/v1/employee/3941",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.message).contains(
+        "Successfully! Record has been fetched."
+      );
+    });
+  });
+});
